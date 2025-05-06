1c1f1ae2aec797d8d1856d391475b3514f889028
Tony Geier <tony.18@hotmail.de>
2025-05-06 09:47:03 +0200

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,29 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+namespace ValheimBetterTames
+{
+    [BepInPlugin("TeamKoro.BetterTames", "BetterTames", "0.0.1")]
+    [BepInProcess("valheim.exe")]
+    public class ValheimMod : BaseUnityPlugin
+    {
+        private readonly Harmony harmony = new Harmony("dickdangerjustice.ValheimMod");
+
+        void Awake()
+        {
+            harmony.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(Character), nameof(Character.Jump))]
+        class Jump_Patch
+        {
+            static void Prefix(ref float ___m_jumpForce)
+            {
+                Debug.Log($"Jump force: {___m_jumpForce}");
+                ___m_jumpForce = 15;
+                Debug.Log($"Modified jump force: {___m_jumpForce}");
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Valheim.BetterTames")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Atlas Playbook v0.4.1")]
+[assembly: AssemblyProduct("Valheim.BetterTames")]
+[assembly: AssemblyCopyright("Copyright © Atlas Playbook v0.4.1 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c7a9c4aa-0b32-47f7-a590-a68e94a2511e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
